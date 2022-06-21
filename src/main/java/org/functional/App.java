3491314a95efe9362c5b9ab8a0f4ad1d5789ad4f
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-21 21:42:17 +0200

    Initial commit

---
+++ src/main/java/org/functional/App.java
@@ -0,0 +1,13 @@
+package org.functional;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Functional Programming!" );
+    }
+}
