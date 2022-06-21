2a5cb80e0add9bbda2a69ad93a0089cb975036e6
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-21 21:56:26 +0200

    Lambda expression assigned to simple functional interface

--- src/main/java/org/functional/App.java
+++ src/main/java/org/functional/App.java
@@ -1,5 +1,7 @@
 package org.functional;
 
+import org.functional.interfaces.MyValue;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Functional Programming!" );
+        System.out.println( "Hello World Functional Programming!\r\n" );
+
+        // lambda expression assigned to the simple functional interface reference
+        MyValue myValue;
+        myValue = () -> 98.6;
+        System.out.println("A constant value: " + myValue.getValue());
     }
 }
