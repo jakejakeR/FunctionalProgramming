6ec044bd109a1e845271f77d08b1fb91e99629a4
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-21 22:16:50 +0200

    Logger added.

--- src/main/java/org/functional/App.java
+++ src/main/java/org/functional/App.java
@@ -1,13 +1,15 @@
 package org.functional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World Functional Programming!" );
+public class App {
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger("App");
+        logger.info("Hello World Functional Programming!");
     }
 }
