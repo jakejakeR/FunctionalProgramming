69bd8f55eed8e37153b32a60051dd28c1ddcd398
Jakub Rogowski <jakerogowski33@gmail.com>
2022-06-22 17:08:14 +0200

    Ex3 - Created simple Monad from Container class

--- src/main/java/org/functional/containers/Container.java
+++ src/main/java/org/functional/containers/Container.java
@@ -11,11 +11,23 @@ public class Container<T> {
     }
 
     /**
-     * Returns new Container of type U.
-     * Uses Functional interface that "swaps" type T to type U.
+     * "Swaps" type T to type U.
+     * @param mapper
+     * @return
+     * @param <U>
      */
     public <U> Container<U> map(MyFirstFunction<T, U> mapper) {
-        return new Container<U>(mapper.apply(value));
+        return new Container<>(mapper.apply(value));
+    }
+
+    /**
+     * "Swaps" type T to Container of type U
+     * @param mapper
+     * @return
+     * @param <U>
+     */
+    public <U> Container<U> flatMap(MyFirstFunction<T, Container<U>> mapper) {
+        return mapper.apply(value);
     }
 
     public T getValue() {
