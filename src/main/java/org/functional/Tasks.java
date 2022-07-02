e3606e5fe4c5067f1580b26aca8dda7617595bea
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-02 17:17:39 +0200

    Sorted descending using Comparator.reverseOrder

--- src/main/java/org/functional/Tasks.java
+++ src/main/java/org/functional/Tasks.java
@@ -1,5 +1,6 @@
 package org.functional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -7,7 +8,7 @@ import java.util.stream.IntStream;
 public class Tasks {
     public static void main(String[] args) {
 
-        List<Integer> numbersDivisibleBySix = IntStream.range(0, 101).boxed().filter(s -> s % 6 == 0).sorted((a, b) -> b.compareTo(a)).collect(Collectors.toList());
+        List<Integer> numbersDivisibleBySix = IntStream.range(0, 101).boxed().filter(s -> s % 6 == 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         System.out.println(numbersDivisibleBySix);
     }
 }
