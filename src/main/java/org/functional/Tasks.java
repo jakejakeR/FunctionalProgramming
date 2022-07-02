1c8716ecc46f3adce2763fc3352bb1ce456575e5
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-02 17:17:10 +0200

    Generating numbers divisible by 6, sorted descending

--- src/main/java/org/functional/Tasks.java
+++ src/main/java/org/functional/Tasks.java
@@ -1,17 +1,13 @@
 package org.functional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Tasks {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger("App");
 
-        List<Integer> numbersDivisibleBySix = IntStream.range(0, 101).boxed().filter(s -> s % 6 == 0).collect(Collectors.toList());
+        List<Integer> numbersDivisibleBySix = IntStream.range(0, 101).boxed().filter(s -> s % 6 == 0).sorted((a, b) -> b.compareTo(a)).collect(Collectors.toList());
         System.out.println(numbersDivisibleBySix);
     }
 }
