c239865ada03fa5089382dd7b83f3b23c27bf55a
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-02 16:20:19 +0200

    Optional revision

---
+++ src/main/java/org/functional/Tasks.java
@@ -0,0 +1,22 @@
+package org.functional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+public class Tasks {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger("App");
+
+        String x = "Hello!";
+        Optional<String> maybeX = Optional.ofNullable(x);
+        logger.info(maybeX.toString());
+
+        Optional<Integer> maybeInt = maybeX.map(s -> s.length()).filter(s -> s % 2 == 0);
+        logger.info(maybeInt.toString());
+
+        Integer myValue = maybeInt.orElse(0);
+        logger.info(myValue.toString());
+    }
+}
