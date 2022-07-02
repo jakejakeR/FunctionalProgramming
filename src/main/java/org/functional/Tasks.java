ebd52e15426a23c84c458400eddce9d4ce9428a3
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-02 17:42:48 +0200

    Collecting

--- src/main/java/org/functional/Tasks.java
+++ src/main/java/org/functional/Tasks.java
@@ -1,23 +1,33 @@
 package org.functional;
 
-import java.util.Comparator;
+import org.functional.model.Address;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Tasks {
     public static void main(String[] args) {
 
-        List<Integer> numbersDivisibleBySix = IntStream
-                .range(0, 101)
-                .boxed()
-                .filter(s -> s % 6 == 0)
-                .sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        List<Address> addressList = new ArrayList<>();
+        addressList.add(new Address("Pomorskie", "Gdansk", "Dluga", 200));
+        addressList.add(new Address("Pomorskie", "Gdansk", "Hallera", 1005));
+        addressList.add(new Address("Pomorskie", "Gdansk", "Partyzantow", 1000));
+        addressList.add(new Address("Malopolskie", "Krakow", "Szeroka", 2000));
+        addressList.add(new Address("Mazowieckie", "Warszawa", "Jerozolimskie", 500));
+        addressList.add(new Address("Pomorskie", "Pruszcz", "Chopina", 50));
+        addressList.add(new Address("Pomorskie", "Gdynia", "Swietojanska", 90));
+        addressList.add(new Address("Malopolskie", "Zakopane", "Krupowki", 30));
+        addressList.add(new Address("Lodzkie", "Lodz", "Piotrkowska", 100));
 
-        Optional<Integer> optionalSum = numbersDivisibleBySix.stream().reduce(Integer::sum);
-        Integer sumOfIntegers = optionalSum.orElse(0);
-        System.out.println("Sum of " + numbersDivisibleBySix);
-        System.out.println("is: " + sumOfIntegers);
+        Map<String, List<Address>> collect = addressList.stream().collect(Collectors.groupingBy(Address::getState));
+        System.out.println(collect);
+
+        List<String> cities = addressList.stream().map(Address::getCity).distinct().collect(Collectors.toList());
+        System.out.println(cities);
+
+        String citiesAsString = addressList.stream().map(Address::getCity).distinct().collect(Collectors.joining(","));
+        System.out.println(citiesAsString);
     }
 }
