5876fbef5b0d862fe536d19d57c151f159a11c79
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-02 17:24:12 +0200

    Reduce the sorted integer list

--- src/main/java/org/functional/Tasks.java
+++ src/main/java/org/functional/Tasks.java
@@ -2,13 +2,22 @@ package org.functional;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Tasks {
     public static void main(String[] args) {
 
-        List<Integer> numbersDivisibleBySix = IntStream.range(0, 101).boxed().filter(s -> s % 6 == 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
-        System.out.println(numbersDivisibleBySix);
+        List<Integer> numbersDivisibleBySix = IntStream
+                .range(0, 101)
+                .boxed()
+                .filter(s -> s % 6 == 0)
+                .sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+
+        Optional<Integer> optionalSum = numbersDivisibleBySix.stream().reduce((accumulator, number) -> accumulator + number);
+        Integer sumOfIntegers = optionalSum.orElse(0);
+        System.out.println("Sum of " + numbersDivisibleBySix);
+        System.out.println("is: " + sumOfIntegers);
     }
 }
