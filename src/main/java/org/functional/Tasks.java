98b0094fc426ddd3dd8440184bc6b7bd6b848eab
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-02 17:06:22 +0200

    IntStream generating million Integers using range()

--- src/main/java/org/functional/Tasks.java
+++ src/main/java/org/functional/Tasks.java
@@ -3,28 +3,16 @@ package org.functional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Tasks {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger("App");
 
-        List<String> names = new ArrayList<>();
-        names.add("Anna");
-        names.add("Jake");
-        names.add("Juliet");
-        names.add("Mathew");
-        names.add("Jason");
-        names.add("Brandon");
-        names.add("Winnie");
-        names.add("Rex");
-        names.add("Dominic");
-
-        List<String> sortedNames = names.stream().sorted(((o1, o2) -> o1.compareToIgnoreCase(o2))).collect(Collectors.toList());
-
-        logger.info(sortedNames.toString());
+        List<Integer> millionIntegers = IntStream.range(0, 1000000).boxed().collect(Collectors.toList());
+        System.out.println(millionIntegers.size());
 
     }
 }
