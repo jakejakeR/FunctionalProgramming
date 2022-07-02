98f35b569065fdba42ffce396b82b19b3d7cfaf2
Jakub Rogowski <jakerogowski33@gmail.com>
2022-07-02 17:01:20 +0200

    Sorting using compareTo

--- src/main/java/org/functional/Tasks.java
+++ src/main/java/org/functional/Tasks.java
@@ -1,27 +1,30 @@
 package org.functional;
 
-import org.functional.model.Address;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Tasks {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger("App");
 
-        List<Address> addressList = new ArrayList<>();
-        addressList.add(new Address("Pomorskie", "Gdansk", "Dluga", 200));
-        addressList.add(new Address("Pomorskie", "Gdansk", "Hallera", 1005));
-        addressList.add(new Address("Pomorskie", "Gdansk", "Partyzantow", 1000));
-        addressList.add(new Address("Malopolskie", "Krakow", "Szeroka", 2000));
-        addressList.add(new Address("Mazowieckie", "Warszawa", "Jerozolimskie", 500));
-        addressList.add(new Address("Pomorskie", "Pruszcz", "Chopina", 50));
-        addressList.add(new Address("Pomorskie", "Gdynia", "Swietojanska", 90));
-        addressList.add(new Address("Malopolskie", "Zakopane", "Krupowki", 30));
-        addressList.add(new Address("Lodzkie", "Lodz", "Piotrkowska", 100));
+        List<String> names = new ArrayList<>();
+        names.add("Anna");
+        names.add("Jake");
+        names.add("Juliet");
+        names.add("Mathew");
+        names.add("Jason");
+        names.add("Brandon");
+        names.add("Winnie");
+        names.add("Rex");
+        names.add("Dominic");
 
-        addressList.stream().forEach(address -> logger.info(address.getState()));
+        List<String> sortedNames = names.stream().sorted(((o1, o2) -> o1.compareToIgnoreCase(o2))).collect(Collectors.toList());
+
+        logger.info(sortedNames.toString());
+
     }
 }
